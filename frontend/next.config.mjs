c9015cde79abfaabf169d30d989882dad4c4f019
sarthakjdev <jsarthak448@gmail.com>
2025-02-10 17:27:54 +0530

    fix: env fix
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,8 +7,8 @@ console.log('isCloudEdition', isCloudEdition)
 const nextConfig = {
 	env: {
 		NEXT_PUBLIC_IS_MANAGED_CLOUD_EDITION: process.env.NEXT_PUBLIC_IS_MANAGED_CLOUD_EDITION,
-		BACKEND_URL: process.env.BACKEND_URL,
-		RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID
+		BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+		RAZORPAY_KEY_ID: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID
 	},
 	output: !isCloudEdition ? 'export' : undefined,
 	images: {
