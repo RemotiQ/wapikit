96f833a4e45ce53a579d176cc9ba761604ce94d8
sarthakjdev <jsarthak448@gmail.com>
2025-02-10 19:40:25 +0530

    fix: use once for db connection instance and fine tune a bit
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- internal/database/db.go
+++ internal/database/db.go
@@ -2,22 +2,41 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"sync"
+	"time"
 
 	_ "github.com/go-jet/jet/v2"
 	_ "github.com/lib/pq"
 )
 
-var DatabaseConnection *sql.DB
+var (
+	DatabaseConnection *sql.DB
+	once               sync.Once
+)
 
 func GetDbInstance(dbUrl string) *sql.DB {
-	if DatabaseConnection != nil {
-		return DatabaseConnection
-	}
-	dsn := dbUrl
-	var err error
-	DatabaseConnection, err = sql.Open("postgres", dsn)
-	if err != nil {
-		panic(err)
-	}
+	once.Do(func() {
+		fmt.Println("Initializing Database Connection...")
+		var err error
+		DatabaseConnection, err = sql.Open("postgres", dbUrl)
+		if err != nil {
+			panic(err)
+		}
+
+		DatabaseConnection.SetMaxOpenConns(50)
+		DatabaseConnection.SetMaxIdleConns(20)
+		DatabaseConnection.SetConnMaxLifetime(30 * time.Minute)
+		DatabaseConnection.SetConnMaxIdleTime(5 * time.Minute)
+
+		err = DatabaseConnection.Ping()
+		if err != nil {
+			log.Fatalf("Failed to ping database: %v", err)
+		}
+
+		fmt.Println("Database Connection Successful")
+	})
+
 	return DatabaseConnection
 }
