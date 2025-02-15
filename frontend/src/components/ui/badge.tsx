50c4a191e67df0ba8d1d3e2fd826c53e833c25c8
Nandani Paliwal <reach.nandanipaliwal@gmail.com>
2025-02-15 15:14:21 +0530

    fix: wip [skip ci]
    Signed-off-by: Nandani Paliwal <reach.nandanipaliwal@gmail.com>

--- frontend/src/components/ui/badge.tsx
+++ frontend/src/components/ui/badge.tsx
@@ -4,12 +4,12 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { clsx as cn } from 'clsx'
 
 const badgeVariants = cva(
-	'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold cursor-default',
+	'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold cursor-default transform transition-colors',
 	{
 		variants: {
 			variant: {
 				default: 'border-transparent bg-primary text-primary-foreground shadow',
-				secondary: 'border-transparent bg-secondary text-secondary-foreground',
+				secondary: 'border-transparent bg-gray-300 text-secondary-foreground',
 				destructive: 'border-transparent bg-destructive text-destructive-foreground shadow',
 				outline: 'text-foreground',
 				highlighted: 'bg-primary border border-primary text-white shadow px-[5px]'
